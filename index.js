ac337f50838b0e9ec8da05ebd108dd50bc797928
ericalandouglas <ericalandouglas@gmail.com>
2016-04-10 03:27:43 -0700

    reorganized modules, added logging

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 'use strict';
 
-var download = require('./lib/download.js');
-
-var archiveDir = require('./lib/archive.js');
+var download = require('./lib/download');
+var logger = require('./lib/logger.js')();
 
 var standardCb = function (err, data) {
   if (err) {
-    console.log(err)
-    console.log('error');
+    logger.error('error:', err);
   } else {
-    console.log(data[3][1].Location);
-    console.log('success');
+    logger.info("Archive available for dl at", data[3][1].Location);
   }
 };
 
-download.getUserLikes('https://soundcloud.com/kyle-savidge-1', standardCb);
+//download.getUserLikes('https://soundcloud.com/edouglastest', standardCb);
 //download.getPlaylist('https://soundcloud.com/edouglastest/sets/test_playlist', standardCb);
-
 
