776f590597c1685ebfb8513f72cc84e7cb429490
ericalandouglas <ericalandouglas@gmail.com>
2016-04-09 23:40:41 -0700

    Finished methods for playlist and favorite uploading.

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 
 var download = require('./lib/download.js');
 
-var standardCb = function (err) {
+var archiveDir = require('./lib/archive.js');
+
+var standardCb = function (err, data) {
   if (err) {
     console.log(err)
     console.log('error');
   } else {
-    console.log('success')
+    console.log(data[3][1].Location);
+    console.log('success');
   }
 };
 
-download.getUserLikes('https://soundcloud.com/edouglastest', __dirname + '/playlist_test', standardCb);
+download.getUserLikes('https://soundcloud.com/kyle-savidge-1', standardCb);
+//download.getPlaylist('https://soundcloud.com/edouglastest/sets/test_playlist', standardCb);
+
 
