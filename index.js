78a78a29d961c377e309b40f8a920344e0b2b6d1
ericalandouglas <ericalandouglas@gmail.com>
2016-04-09 21:50:00 -0700

    Adding archive functionality

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 'use strict';
 
-var writeFavorites = require('./lib/writeFavorites.js');
-var writePlaylist = require('./lib/writePlaylist.js');
-var downloadUrls = require('./lib/downloadUrl.js');
+var download = require('./lib/download.js');
 
 var standardCb = function (err) {
   if (err) {
     console.log(err)
+    console.log('error');
   } else {
     console.log('success')
   }
 };
 
-//downloadUrls('myplaylist.txt', __dirname + '/playlist_test', standardCb);
-//writeFavorites('someked', 'myfavs.txt', standardCb);
-//writePlaylist('https://soundcloud.com/sheltereddougie/sets/beat-culture', 'myplaylist.txt', standardCb);
+download.getUserLikes('https://soundcloud.com/edouglastest', __dirname + '/playlist_test', standardCb);
 
