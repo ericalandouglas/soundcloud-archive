d100abbc498b3a55d4e2b0d13dac628ace01aa00
ericalandouglas <ericalandouglas@gmail.com>
2016-04-10 19:22:23 -0700

    added module to cluster downloading

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 'use strict';
 
-var download = require('./lib/download');
-var logger = require('./lib/logger.js')();
+var cluster = require('./lib/cluster.js');
 
-var standardCb = function (err, data) {
-  if (err) {
-    logger.error('error:', err);
-  } else {
-    logger.info("Archive available for dl at", data[3][1].Location);
-  }
-};
-
-//download.getUserLikes('https://soundcloud.com/someked', standardCb);
-//download.getPlaylist('https://soundcloud.com/sheltereddougie/sets/beat-culture', standardCb);
+cluster();
 
